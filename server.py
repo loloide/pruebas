60a9546af3c38fc1538fc241fe033bb9b7a44c8e
loloide <75491222+loloide@users.noreply.github.com>
2021-02-12 00:20:47 -0300

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,19 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(404)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(bytes('{"a":1}', "utf-8"))
+        self.wfile.write(bytes(["martin,silva,36",
+                                "lorenzo,de la cruz,15",
+                                "koyomi,araragi,17",
+                                "suruga,kanbaru,16",
+                                "izuko,gaen,26",
+                                "ocelot,revolver,68,shalashaska",
+                                "sodachi,oikura,17",
+                                "shinobu,oshino,500+",
+                                "asriel,dremurr,20",
+                                "nadeko,sengoku,15",
+                                "kaiki,deishuu,40",
+                                "solid,skane,24",
+                                "hola,soy german,100"]))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
