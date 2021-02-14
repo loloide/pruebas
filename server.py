9f30d5ee71c88f0e9244771912ecc55005ea41a3
loloide <lorendlc2005@gmail.com>
2021-02-14 19:41:27 -0300

    salio al fin

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(bytes("hola", "utf-8"))
+        self.wfile.write(bytes(database, "utf-8"))
 
 
 if __name__ == "__main__":        
